40956f33c3770b30895b00b12462058d9e68e7e0
mohamed255255 <mohamedgaber255255@gmail.com>
2025-08-31 15:36:04 +0300

    fixed bugs in SecurityConfiguration

--- src/main/java/com/garage_system/Security/SecurityConfiguration.java
+++ src/main/java/com/garage_system/Security/SecurityConfiguration.java
@@ -3,39 +3,47 @@ package com.garage_system.Security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import static org.springframework.security.config.Customizer.withDefaults;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import com.garage_system.Service.CustomUserDetailsService;
 
 @Configuration
 public class SecurityConfiguration {
 
-    @Autowired
     private final CustomUserDetailsService customUserDetailsService;
-
-    public SecurityConfiguration(CustomUserDetailsService customUserDetailsService) {
+    
+    private final JWTFilter JwtFilter ;
+    
+    public SecurityConfiguration(CustomUserDetailsService customUserDetailsService  , JWTFilter JwtFilter) {
         this.customUserDetailsService = customUserDetailsService;
+        this.JwtFilter = JwtFilter ;
     }
 
-    @Bean
+        @Bean
         public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
             http
-               .csrf(csrf -> csrf.disable())
+              .csrf(csrf -> csrf.disable())
+              .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
               .authorizeHttpRequests(auth -> auth
                    .requestMatchers("/Register" , "/login").permitAll()  /// white list for guest users
                    .anyRequest().authenticated()
               )
-              .httpBasic(withDefaults()); 
+              .addFilterBefore(JwtFilter, UsernamePasswordAuthenticationFilter.class)
+              .httpBasic(withDefaults());
 
             return http.build();
         }
-
+        
     @Bean
     public AuthenticationProvider authenticationProvider() {
         DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
@@ -43,7 +51,11 @@ public class SecurityConfiguration {
         provider.setPasswordEncoder(passwordEncoder());       
         return provider;
     }
-
+ 
+    @Bean
+    public  AuthenticationManager AuthenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
--- src/main/java/com/garage_system/Service/UserService.java
+++ src/main/java/com/garage_system/Service/UserService.java
@@ -1,6 +1,5 @@
 package com.garage_system.Service;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.garage_system.Model.User;
@@ -11,7 +10,6 @@ public class UserService {
     
     private final UserRepository userRepository;
      
-     @Autowired
      public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
